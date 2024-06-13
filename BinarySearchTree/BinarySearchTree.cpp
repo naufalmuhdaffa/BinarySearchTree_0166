a2409277c8b404edd823f7a7d0ff972c42137520
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-06-13 11:28:00 +0700

    menambahkan method

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -99,6 +99,21 @@ public:
             preorder(ptr->rightchild);
         }
     }
+
+    void postorder(Node* ptr)
+    {
+        if (ROOT == nullptr)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
 };
 
 int main()
